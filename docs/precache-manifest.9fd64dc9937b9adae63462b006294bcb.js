a33ce6a69d1f04cf910cdf2cc68313d7ce85492e
Julio Flores <juliocesarflores12@gmail.com>
2020-09-29 04:26:29 -0400

    docs agregado

---
+++ docs/precache-manifest.9fd64dc9937b9adae63462b006294bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5857ad7cd4084499727711ffbe4a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2dd9751ab994409aa93",
+    "url": "/static/css/main.a2f41507.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dd9751ab994409aa93",
+    "url": "/static/js/main.b22c4d66.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
